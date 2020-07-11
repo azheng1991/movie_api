ab83b468f353af11d782b8b9334069a5adab7f78
Allan Zheng <azheng34@outlook.com>
2020-07-11 12:52:55 -0400

    test

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ app.post(
       return res.status(422).json({ errors: errors.array() })
     }
 
-    let hashedPassword = Users.hashPassword(req.body.Password)
+    let hashedPassword = Users.HashPassword(req.body.Password)
     Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
       .then((user) => {
         if (user) {
