583293528235bbce5fd8fc3afaeb5845419d1441
Allan Zheng <azheng34@outlook.com>
2020-07-11 19:16:32 -0400

    connection link

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, 
 */
 
 Connect to online database hosted on MongoDB Atlas
-mongoose.connect('mongoose.connect( process.env.CONNECTION_URI,' { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://azheng:celtics88@myflixdb.6a2km.mongodb.net/myFlixDB?retryWrites=true&w=majority,' { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
