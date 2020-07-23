dd235d96199af15b076a7e1c7071ee2c0011afbb
Allan Zheng <azheng34@outlook.com>
2020-07-22 20:50:07 -0400

    difficulty with filter

--- client/src/components/movies-list/movies-list.jsx
+++ client/src/components/movies-list/movies-list.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
-import { MovieCard } from '../movie-card/movie-card';
-import Col from 'react-bootstrap/Row';
-import Row from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
+import { MovieCard } from "../movie-card/movie-card";
+// import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import "./movies-list.scss";
 
 const mapStateToProps = (state) => {
   const { visibilityFilter } = state;
@@ -16,26 +19,33 @@ function MoviesList(props) {
   const { movies, visibilityFilter } = props;
   let filteredMovies = movies;
 
-  if (visibilityFilter !== '') {
-    filteredMovies = movies.filter((m) => m.Title.includes(visibilityFilter));
+  if (visibilityFilter !== "") {
+    filteredMovies = movies.filter((m) =>
+      m.Title.toLocaleLowerCase().includes(visibilityFilter.toLocaleLowerCase())
+    );
   }
 
   if (!movies) return <div className="main-view" />;
 
   return (
     <div className="movies-list">
+      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+
       <Container>
+        {/* <Col> */}
         <Row>
-          <Col>
-            <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-            {filteredMovies.map((m) => (
-              <MovieCard key={m._id} movie={m} />
-            ))}
-          </Col>
+          {filteredMovies.map((m) => (
+            <MovieCard key={m._id} movie={m} />
+          ))}
         </Row>
+        {/* </Col> */}
       </Container>
     </div>
   );
 }
+
+MoviesList.propTypes = {
+  visibilityFilter: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps)(MoviesList);
